cbb8dea3a74c3607f6791e151e9eee7abb2be03b
Mortadha Toffaha <mortadha.toffaha@amadeus.com>
2015-02-03 17:10:30 +0100

    Added Helper for running remote Sparql queries to LinkedMDB, and resolved the Jena dependencies issue

--- webservice/src/main/java/org/polytech/unice/websem/wisecat/controller/RecommandationController.java
+++ webservice/src/main/java/org/polytech/unice/websem/wisecat/controller/RecommandationController.java
@@ -1,5 +1,9 @@
 package org.polytech.unice.websem.wisecat.controller;
 
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
 import org.polytech.unice.websem.wisecat.model.Movie;
 import org.polytech.unice.websem.wisecat.model.Person;
 import org.springframework.stereotype.Controller;
@@ -33,8 +37,12 @@ public class RecommandationController {
 			Movie mv = new Movie(movieTitles[i],Math.abs((random.nextInt()+90) % 200),"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",posters[i]);
 			mv.setGenres(getRandomGenre());
 			recommandations.add(mv);
+			mv.setPoster(posters[i]);
 			mv.setActors(getRandomCast());
 		}
+
+
+
 		return recommandations;
 
 	}
