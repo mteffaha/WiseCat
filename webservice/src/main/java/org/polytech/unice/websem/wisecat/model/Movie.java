498f57e4700fb50684ed8ede1756bd78d7a3cac6
Mortadha Toffaha <mortadha.toffaha@amadeus.com>
2015-02-07 22:03:05 +0100

    Updated Movie and Person

--- webservice/src/main/java/org/polytech/unice/websem/wisecat/model/Movie.java
+++ webservice/src/main/java/org/polytech/unice/websem/wisecat/model/Movie.java
@@ -7,12 +7,20 @@ import java.util.List;
  * Created by mtoffaha on 03/02/2015.
  */
 public class Movie {
+    private String movieID;
     private String title;
     private List<String> genres = new ArrayList<String>();
     private int duration;
     private String poster;
     private String plot;
     private List<Person> actors = new ArrayList<Person>();
+
+    private List<Person> directors = new ArrayList<Person>();
+    private List<Person> writers = new ArrayList<Person>();
+    private List<Person> editors = new ArrayList<Person>();
+    private List<String> songs = new ArrayList<String>();
+    private List<Person> composers = new ArrayList<Person>();
+
 
     public Movie() {
     }
@@ -71,5 +79,53 @@ public class Movie {
 
     public void setPlot(String plot) {
         this.plot = plot;
+    }
+
+    public String getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(String movieID) {
+        this.movieID = movieID;
+    }
+
+    public List<Person> getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(List<Person> directors) {
+        this.directors = directors;
+    }
+
+    public List<Person> getWriters() {
+        return writers;
+    }
+
+    public void setWriters(List<Person> writers) {
+        this.writers = writers;
+    }
+
+    public List<Person> getEditors() {
+        return editors;
+    }
+
+    public void setEditors(List<Person> editors) {
+        this.editors = editors;
+    }
+
+    public List<String> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<String> songs) {
+        this.songs = songs;
+    }
+
+    public List<Person> getComposers() {
+        return composers;
+    }
+
+    public void setComposers(List<Person> composers) {
+        this.composers = composers;
     }
 }
